49360ecf1f4a24b562d1ed5a8ee80be4c623e9d3
AAcostaAponte <98475675+AAcostaAponte@users.noreply.github.com>
2022-02-01 13:42:00 -0600

    Added some UI functionality
    temp UI prefabs for In game, Main Menu, and the pause screen have been added.

---
+++ ClimbOn_1.0/Assets/Scripts/sUiNavigation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sUiNavigation : MonoBehaviour
+{
+    
+    public void OnPlayButtonPressed()
+    {
+
+        //add a move to new scene code
+
+        Destroy(this.gameObject);
+
+
+    }
+
+
+
+
+
+
+
+}
